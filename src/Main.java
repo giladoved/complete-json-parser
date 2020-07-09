68e8bad8471b913a55225db779283d946ec0ba28
Gilad Oved <giladoved@gmail.com>
2020-07-09 16:29:43 -0500

    checkin

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -8,13 +9,13 @@ public class Main {
     public static void main(String[] args) {
         File baseDir = new File("/Users/giladoved/IdeaProjects/jsonparser/out/production/jsonparser");
 
-        LinkedList<Token> tokens = new LinkedList<>();
+//        LinkedList<Token> tokens = new LinkedList<>();
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(baseDir, "sample.json"))))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(baseDir, "sample3.json"))))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
-                System.out.println("line: " + line);
+//                System.out.println("line: " + line);
 
                 int index = 0;
                 while (index < line.length()) {
@@ -34,18 +35,13 @@ public class Main {
                     } else if (subline.contains(new String(Null.unicode)) && subline.length() >= Null.unicode.length && subline.substring(0, Null.unicode.length).equals(new String(Null.unicode))) {
                         tokens.add(new Null());
                         index += Null.unicode.length;
-                    }
-
-                    else if (numberMatcher.find() && numberMatcher.start() == 0) {
+                    } else if (numberMatcher.find() && numberMatcher.start() == 0) {
                         tokens.add(new NumberToken(subline.substring(numberMatcher.start(), numberMatcher.end())));
                         index += numberMatcher.end();
-                    }
-                    else if (stringMatcher.find() && stringMatcher.start() == 0) {
+                    } else if (stringMatcher.find() && stringMatcher.start() == 0) {
                         tokens.add(new StringToken(subline.substring(stringMatcher.start(), stringMatcher.end())));
                         index += stringMatcher.end();
-                    }
-
-                    else if (currentChar == Colon.unicode) {
+                    } else if (currentChar == Colon.unicode) {
                         tokens.add(new Colon());
                         index++;
                     } else if (currentChar == Comma.unicode) {
@@ -64,20 +60,18 @@ public class Main {
                         tokens.add(new RightSquareBracket());
                         index++;
                     } else if (currentChar == Tab.unicode) {
-                        tokens.add(new Tab());
+//                        tokens.add(new Tab());
                         index++;
                     } else if (currentChar == LineFeed.unicode) {
-                        tokens.add(new LineFeed());
+//                        tokens.add(new LineFeed());
                         index++;
                     } else if (currentChar == CarriageReturn.unicode) {
-                        tokens.add(new CarriageReturn());
+//                        tokens.add(new CarriageReturn());
                         index++;
                     } else if (currentChar == Space.unicode) {
-                        tokens.add(new Space());
+//                        tokens.add(new Space());
                         index++;
-                    }
-
-                    else {
+                    } else {
                         index++;
                     }
                 }
@@ -89,8 +83,212 @@ public class Main {
             System.out.println(tokens);
 
 
+
+            ArrayList<JSO> swags = parse(tokens, 0, tokens.size()-1);
+            for (JSO jso : swags) {
+                System.out.print(jso + " ");
+            }
+//            System.out.println("SWAG: " + swags);
+
+
+
         } catch (IOException e) {
             System.out.println("e: " + e);
+        } catch (Exception e) {
+            System.out.println("PARSING ERR: " + e);
+            e.printStackTrace();
         }
+    }
+
+//    public static LinkedList<Token> tokens = new LinkedList<>();
+    public static ArrayList<Token> tokens = new ArrayList<>();
+//    public static ArrayList<JSO> parsedTokens = new ArrayList<>();
+
+    private static ArrayList<JSO> parse(ArrayList<Token> tokens, int start, int end) throws Exception {
+        ArrayList<JSO> parsedTokens = new ArrayList<>();
+        Token token = tokens.get(start);
+        if (token instanceof True) {
+            parsedTokens.add(new JSO("true"));
+            return parsedTokens;
+        } else if (token instanceof False) {
+            parsedTokens.add(new JSO("false"));
+            return parsedTokens;
+        } else if (token instanceof Null) {
+            parsedTokens.add(new JSO("null"));
+            return parsedTokens;
+        } else if (token instanceof StringToken) {
+            parsedTokens.add(new JSO("String"));
+            return parsedTokens;
+        } else if (token instanceof NumberToken) {
+            parsedTokens.add(new JSO("Number"));
+            return parsedTokens;
+        }
+
+        else if (token instanceof LeftSquareBracket && tokens.get(end) instanceof RightSquareBracket) {
+            parsedTokens.add(new JSO("["));
+            parsedTokens.addAll(parseArray(tokens, start+1, end-1));
+            parsedTokens.add(new JSO("]"));
+            return parsedTokens;
+        }
+
+        else if (token instanceof LeftCurlyBracket && tokens.get(end) instanceof RightCurlyBracket) {
+            parsedTokens.add(new JSO("{"));
+            parsedTokens.addAll(parseObject(tokens, start+1, end-1));
+            parsedTokens.add(new JSO("}"));
+            return parsedTokens;
+        }
+
+//        else if (token instanceof LeftSquareBracket && tokens.get(end) instanceof RightSquareBracket) {
+//            return isValidArray(tokens, start+1, end-1);
+//        } else if (token instanceof LeftCurlyBracket && tokens.get(end) instanceof RightCurlyBracket) {
+//            return isValidObject(tokens, start+1, end-1);
+//        }
+
+//        return false;
+
+        return null;
+    }
+
+    private static ArrayList<JSO> parseObject(ArrayList<Token> tokens, int start, int end) throws Exception {
+        ArrayList<JSO> jsoo = new ArrayList<>();
+        JSOO object = new JSOO();
+
+        while (start + 2 <= end) {
+            if (!(tokens.get(start) instanceof StringToken)) {
+                throw new Exception("NOT A STRING KEY");
+            }
+            else if (!(tokens.get(start + 1) instanceof Colon)) {
+                throw new Exception("NOT A COLON");
+            }
+            else {
+                ArrayList<JSO> jsos = parse(tokens, start+2, end);
+                object.map.put(tokens.get(start).toString(), jsos);
+                start += jsos.size();
+            }
+        }
+
+        jsoo.add(object);
+        return jsoo;
+    }
+
+    private static ArrayList<JSO> parseArray(ArrayList<Token> tokens, int start, int end) throws Exception {
+        ArrayList<JSO> array = new ArrayList<>();
+        while (start <= end) {
+            ArrayList<JSO> jsos = parse(tokens, start, end);
+            array.addAll(jsos);
+
+            if (start + jsos.size() > end) {
+                return array;
+            }
+
+            if (!(tokens.get(start + jsos.size()) instanceof Comma)) {
+                throw new Exception("COMMA EXCEPTION");
+            }
+            else {
+                start += jsos.size() + 1;
+            }
+        }
+        return array;
+    }
+
+//    private static boolean isValid(LinkedList<Token> tokens) {
+//        Token token = tokens.pop();
+//        if (token instanceof True) {
+//            return true;
+//        } else if (token instanceof False) {
+//            return true;
+//        } else if (token instanceof Null) {
+//            return true;
+//        } else if (token instanceof StringToken) {
+//            return true;
+//        } else if (token instanceof NumberToken) {
+//            return true;
+//        }
+//        else if (token instanceof LeftSquareBracket) {
+//            return isValidArray(tokens);
+//        }
+////        else if (token instanceof LeftSquareBracket && tokens.get(end) instanceof RightSquareBracket) {
+////            return isValidArray(tokens, start+1, end-1);
+////        } else if (token instanceof LeftCurlyBracket && tokens.get(end) instanceof RightCurlyBracket) {
+////            return isValidObject(tokens, start+1, end-1);
+////        }
+//
+//        return false;
+//    }
+//
+//    private static boolean isValidArray(LinkedList<Token> tokens) {
+//        if (tokens.getFirst() == null) return false;
+//
+//        Token token = tokens.getFirst();
+//        if (token instanceof RightSquareBracket) {
+//            tokens.pop();
+//            return true;
+//        }
+//
+//        while (true) {
+//            boolean isTrue = isValid(tokens);
+//            if (!isTrue) {
+//                return false;
+//            }
+//            else if (tokens.getFirst() instanceof RightSquareBracket) {
+//                tokens.pop();
+//                return true;
+//            }
+//            else if (!(tokens.getFirst() instanceof Comma)) {
+//                return false;
+//            }
+//            else {
+//                tokens.pop();
+//            }
+//        }
+//    }
+
+//    private static boolean isValidObject(ArrayList<Token> tokens, int start, int end) {
+//        if (start == end) return true;
+//
+//        if (tokens.get(start) instanceof StringToken && tokens.get(start+1) instanceof Colon) {
+//            return isValid(tokens, start+2, end);
+//        }
+//
+//        return false;
+//    }
+}
+
+class JSO {
+    String str = null;
+    public JSO(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+}
+
+class JSOO extends JSO {
+    HashMap<String, Object> map;
+
+    public JSOO() {
+        super(null);
+        map = new HashMap<>();
+    }
+
+    public void put(String key, Object val) throws Exception {
+        if (map.containsKey(key)) {
+            throw new Exception("Duplicate key error");
+        }
+
+        map.put(key, val);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('\n');
+        for (String key : map.keySet()) {
+            sb.append(key + ": " + map.get(key) + '\n');
+        }
+        return sb.toString();
     }
 }
